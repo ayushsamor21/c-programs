76962dbf55b9bbda000327a98a5598f31d51d7d8
ayushsamor21 <ayushsamor21@gmail.com>
2025-03-27 23:25:33 +0530

    Update 6-even-elements-odd-elements-in-array.c

--- array/6-even-elements-odd-elements-in-array.c
+++ array/6-even-elements-odd-elements-in-array.c
@@ -1,1 +1,12 @@
-
+#include <stdio.h>
+int main(){
+    int arr1[5] = {11, 20, 31, 40, 51};
+    for(int i = 0; i <= 4; i++){
+        if(arr1[i] % 2 == 0){
+            printf("index[%d] the even number: %d\n", i, arr1[i]);
+        }else{
+            printf("index[%d] the odd number: %d\n", i, arr1[i]);
+        }
+    }
+    return 0;
+}
