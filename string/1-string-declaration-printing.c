c59286d448963ff989dda75fca45e5a4344b87d9
ayushsamor21 <ayushsamor21@gmail.com>
2025-05-16 22:14:45 +0530

    string declaration printing

---
+++ string/1-string-declaration-printing.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    char name1[] = "ARYAN SINGH";
+    char name2[] = {'a', 'r', 'y','a','n','\0'};
+
+    printf("String name :%s\n", name1);
+    printf("String name2:%s\n", name2);
+    return 0;
+}
