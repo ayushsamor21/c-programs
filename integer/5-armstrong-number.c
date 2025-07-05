96c5e6603ac013838a77bad9bcedf1c055e6318b
ayush samor <ayushsamor21@gmail.com>
2025-07-05 20:28:44 +0530

    armstrong number

--- integer/5-armstrong-number.c
+++ integer/5-armstrong-number.c
@@ -2,10 +2,17 @@
 #include <math.h>
 
 void armstrong(int number){
-    int original = number, remainder = 0;
+    int original = number, remainder = 0, size = 0;
+
+    int temp = number;
+    while(temp != 0){
+        size++;
+        temp /= 10;
+    }
+
     while (number != 0){
         int digit = number % 10;
-        remainder = remainder + pow(digit, 3);
+        remainder = remainder + pow(digit, size);
         number /= 10;
     }
     if (remainder == original){
